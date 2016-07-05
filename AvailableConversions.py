61eda01766afc90c931cbf06294daed291722fd5
Max <maxfooo@gmail.com>
2016-07-05 16:31:30 -0700

    Update AvailableConversions.py

--- AvailableConversions.py
+++ AvailableConversions.py
@@ -4,71 +4,47 @@ Created on Jul 1, 2016
 @author: maxr
 '''
 
-from Utils import *
+from Utils import RadixConversion
 from FileIO import FileIO as FIO
-from ProgramBoronArduinoCode import arduinoCode as AC
-from ParseHexFile import *
-import re
+from HexClass import HexClass
+from FILE_TYPES import *
+
 
 class AvailableConversions(object):
     
     def __init__(self):
         self.rc = RadixConversion()
         self.fio = FIO()
-    
-    def hexToMif(self):
-        self.fio.openFile(exten='.hex', ftypes=[('Hex files', '.hex'), ('all files', '.*')])
-        fromFile = self.fio.getOpenedFile()
-        phf = ParseHexFile(fromFile)
-        self.fio.closeOpened()
-        
-        self.mifFile.write('DEPTH = {};\nWIDTH = {};\n'.format(self.depth,self.width) +
-                           'ADDRESS_RADIX = {};\n'.format(self.address_radix) +
-                           'DATA_RADIX = {};\n'.format(self.data_radix) +
-                           'CONTENT\nBEGIN\n')
-        
-
-        self.mifLineCount = 0
-        for i in range(len(self.hexLen)):
-            tempAddress = []
-            for j in range(int(self.hexLen[i],self.base)):
-                addr = int(self.hexAddr[i],self.base)+j
-                tempAddress.append(addr)
-                if addr > self.mifLineCount:
-                    self.mifLineCount = addr
-            for k in range(len(tempAddress)):
-                self.mifFile.write(str(hex(tempAddress[k])).replace('0x','') +
-                                   '\t:\t' + str(self.hexData[i][k]) + ';\n')
+        self.conversionTable = dict()
+        self.hexClass = HexClass()
+        self.conversionTable.update(self.hexClass.hexConversionTable)
+        self.conversionComplete = False
         
         
-        # Fill in the rest of the addresses with 00
-        if self.mifLineCount < int(self.depth) and self.fillZeros == 1:
-            for i in range(int(self.depth) - self.mifLineCount):
-                    self.mifFile.write(str(hex(self.mifLineCount + i + 1)).replace('0x','') +
-                                   '\t:\t' + '00' + ';\n')
-        
-        self.mifFile.write('END;')
-        
-    def hexToIno(self, dataArrFormat = None, printArr = False):
-        self.fio.openFile(exten='.hex', ftypes=[('Hex files', '.hex'), ('all files', '.*')])
-        fromFile = self.fio.getOpenedFile()
-        phf = ParseHexFile(fromFile)
-        self.fio.closeOpened()
-        
-        if dataArrFormat == None:
-            format = ['dpl', 'dph', 'ndb', 'dat']
+    def check(self, fromFileType, toFileType):
+        if fromFileType in self.conversionTable.keys():
+            if toFileType in self.conversionTable[fromFileType]:
+                return True
+            else:
+                return False
         else:
-            format = dataArrFormat
-        orgHexData = phf.structureHexContents(format)
-        arrayStr, arrLen = lst2d2str(orgHexData, pad=False)
-        
-        self.fio.saveFile(exten='.ino', ftypes=[('Arduino files', '.ino'), ('all files', '.*')])
-        toFile = self.fio.getSavedFile()
-        toFile.write(AC(arrLen, arrayStr, str(format) + " (repeated for however many hex lines where made)"))
-        self.fio.closeSaved()
-        
-        if printArr:
-            print(arrayStr, "\n" + str(format) + " (repeated for however many hex lines where made)")
+            return False
+    
+    def convert(self, fromFileType, toFileType):
+        if self.check(fromFileType, toFileType):
+            if fromFileType == DOT_HEX:
+                self.hexClass.convert(toFileType)
+                self.conversionComplete = True
+        else:
+            self.fileIO.errorPopup('A conversion from {0} to {1} does not exist'.format(self.fromFileType, self.toFileType))
+            self.conversionComplete = False
+    
+    
+    def getConversionTable(self):
+        return self.conversionTable
+    
+    def wasSuccessful(self):
+        return self.conversionComplete
         
         
         
